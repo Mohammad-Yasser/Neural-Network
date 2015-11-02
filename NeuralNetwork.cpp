600bf0c0e308f91f560e8fbf111d58df900fc76d
Mohammad Yasser <mohammadyasser3@gmail.com>
2015-11-02 23:27:36 +0200

    Update and rename Neural-Networks.cpp to NeuralNetwork.cpp

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -1,33 +1,23 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include "NeuralNetwork.h"
 
 #define eps 1e-9
-
-int m, l, n;
-// m = input layer nodes, l = hidden layer nodes, n = output layer nodes.
-// iterators on input, hidden, output layers are i,j,k respectively.
-vector<vector<double> > inputWeights, hiddenWeights;
-vector<double> hiddenBias, outputBias;
-double learningRate = 0.001;
-
-double activationFunction(double netI) {
+double NeuralNetwork::activationFunction(double netI) {
 
 }
-bool isDiffFn = 0;
-double dydx(double x) {
+double NeuralNetwork::dydx(double x) {
 	return (activationFunction(x + eps) - activationFunction(x - eps)) / (2 * eps);
 }
-double sum(vector<double> &input, vector<double> &weights) {
+double NeuralNetwork::sum(vector<double> &input, vector<double> &weights) {
 	double netI = 0;
 	for (int i = 0; i < input.size(); ++i)
 		netI += input[i] * weights[i];
 	return netI;
 }
-double evaluate(vector<double> &input, vector<double> &weights) {
+double NeuralNetwork::evaluate(vector<double> &input, vector<double> &weights) {
 	return activationFunction(sum(input, weights));
 }
 
-void train(vector<vector<double> > &input, vector<vector<double> > &output) {
+void NeuralNetwork::train(vector<vector<double> > &input, vector<vector<double> > &output) {
 	for (int j = 0; j < l; ++j) {
 		inputWeights[j].resize(m);
 		for (int i = 0; i < m; ++i)
@@ -50,12 +40,15 @@ void train(vector<vector<double> > &input, vector<vector<double> > &output) {
 		error = 0;
 		for (int testcase = 0; testcase < input.size(); ++testcase) {
 			for (int j = 0; j < l; ++j)
-				hiddenValues[j] = activationFunction(hiddenBias[j] + sum(input[testcase], inputWeights[j]));
+				hiddenValues[j] = activationFunction(
+				  hiddenBias[j] + sum(input[testcase], inputWeights[j]));
 			for (int k = 0; k < m; ++k) {
-				outputValues[k] = activationFunction(outputBias[k] + sum(hiddenValues, hiddenWeights[k]));
+				outputValues[k] = activationFunction(
+				  outputBias[k] + sum(hiddenValues, hiddenWeights[k]));
 				outputDelta[k] = (output[testcase][k] - outputValues[k])
-						* dydx(outputBias[k] + sum(hiddenValues, hiddenWeights[k]));
-				error += (output[testcase][k] - outputValues[k]) * (output[testcase][k] - outputValues[k]);
+				  * dydx(outputBias[k] + sum(hiddenValues, hiddenWeights[k]));
+				error += (output[testcase][k] - outputValues[k])
+				  * (output[testcase][k] - outputValues[k]);
 			}
 			for (int j = 0; j < l; ++j) {
 				for (int k = 0; k < n; ++k)
@@ -74,21 +67,4 @@ void train(vector<vector<double> > &input, vector<vector<double> > &output) {
 			}
 		}
 	}
-}
-
-int main() {
-	srand(time(0));
-	int k; // number of training examples.
-	cin >> m >> l >> n >> k;
-	vector<vector<double> > inExamples(k), outExamples(k);
-	inputWeights.resize(l), hiddenWeights.resize(n);
-	for (int i = 0; i < k; ++i) {
-		inExamples[i].resize(m);
-		outExamples[i].resize(n) ;
-		for (int j = 0; j < m; ++j)
-			cin >> inExamples[i][j];
-		for (int j = 0; j < n; ++j)
-			cin >> outExamples[i][j];
-	}
-	train(inExamples,outExamples) ;
 }
